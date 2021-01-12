75fe5dcc97c00e6c33a6960b5ebcaef134df0790
RaisanJMR <raisanjmr@protonmail.com>
2021-01-12 21:23:01 +0530

    selected DOM elements

---
+++ javascript_array_methods/script.js
@@ -0,0 +1,10 @@
+const main =document.getElementById('main');
+const addUserBtn =document.getElementById('add-user');
+const doubleBtn =document.getElementById('double');
+const showMillionaries =document.getElementById('show-millionaries');
+const sortBtn =document.getElementById('sort');
+const calculateWealthBtn =document.getElementById('calculate-wealth');
+
+let data = [];
+
+//fetch random user and add money
