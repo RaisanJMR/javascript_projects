d3c9cb88a0ee69877121eb2ad7b81c04ffd247ea
RaisanJMR <raisanjmr@protonmail.com>
2021-01-13 18:10:07 +0530

    async await added

--- javascript_array_methods/script.js
+++ javascript_array_methods/script.js
@@ -1,10 +1,27 @@
-const main =document.getElementById('main');
-const addUserBtn =document.getElementById('add-user');
-const doubleBtn =document.getElementById('double');
-const showMillionaries =document.getElementById('show-millionaries');
-const sortBtn =document.getElementById('sort');
-const calculateWealthBtn =document.getElementById('calculate-wealth');
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionaries = document.getElementById("show-millionaries");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
 
 let data = [];
 
-//fetch random user and add money
+getRandomUser();
+getRandomUser();
+getRandomUser();
+//Fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+  addData(newUser);
+}
+// Add new obj to data array
+function addData(obj) {
+  data.push(obj);
+}
