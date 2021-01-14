83af8441ee6511f05e9cfc50c2a05ecb82459501
RaisanJMR <raisanjmr@protonmail.com>
2021-01-14 18:22:44 +0530

    added forEachLoop

--- javascript_array_methods/script.js
+++ javascript_array_methods/script.js
@@ -1,9 +1,9 @@
-const main = document.getElementById("main");
-const addUserBtn = document.getElementById("add-user");
-const doubleBtn = document.getElementById("double");
-const showMillionaries = document.getElementById("show-millionaries");
-const sortBtn = document.getElementById("sort");
-const calculateWealthBtn = document.getElementById("calculate-wealth");
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionaries = document.getElementById('show-millionaries');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
 
 let data = [];
 
@@ -12,7 +12,7 @@ getRandomUser();
 getRandomUser();
 //Fetch random user and add money
 async function getRandomUser() {
-  const res = await fetch("https://randomuser.me/api");
+  const res = await fetch('https://randomuser.me/api');
   const data = await res.json();
   const user = data.results[0];
   const newUser = {
@@ -24,4 +24,26 @@ async function getRandomUser() {
 // Add new obj to data array
 function addData(obj) {
   data.push(obj);
+  updateDOM();
 }
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // clear main div
+  main.innerHTML = `<h2><strong>Person</strong> Wealth</h2>`;
+  providedData.forEach((item) => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+// format number as money
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// event listeners
+addUserBtn.addEventListener('click', getRandomUser);
